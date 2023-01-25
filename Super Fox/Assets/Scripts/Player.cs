cd02b8fe320c655ffc82f9515f77857b0c8a25fb
ArmandoRosasB <armando.rosas133@gmail.com>
2023-01-24 22:33:58 -0600

    Double jumo

--- Super Fox/Assets/Scripts/Player.cs
+++ Super Fox/Assets/Scripts/Player.cs
@@ -6,11 +6,13 @@ public class Player : MonoBehaviour
 {
     public float jumpingStrength;
     
+    private int jump_count;
     private Rigidbody2D rigidbody2D;
     private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        jump_count = 0;
         animator = GetComponent<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
@@ -19,14 +21,28 @@ public class Player : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space)){
-            animator.SetBool("isJumping", true);
-            rigidbody2D.AddForce(new Vector2(0,jumpingStrength));
+            if (jump_count == 0){
+                animator.SetBool("isJumping", true);
+                rigidbody2D.AddForce(new Vector2(0,jumpingStrength));
+                jump_count++;
+            }
+            else if (jump_count == 1){
+                animator.SetBool("isJumpingHigher", true);
+                rigidbody2D.AddForce(new Vector2(0,jumpingStrength));
+                jump_count++;
+            }
+
+            else{
+
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.tag == "Floor"){
             animator.SetBool("isJumping", false);
+            animator.SetBool("isJumpingHigher", false);
+            jump_count = 0;
         }
     }
 }
